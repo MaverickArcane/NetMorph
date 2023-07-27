52b8bf312beafd4ae048d82831dfe9bda2d96dbc
Diego Briceno <diego@arcanesecurity.org>
2023-07-26 20:12:20 -0700

    Added initial working code

---
+++ Dodger.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/sudo /bin/bash
+
+if [ -z $1 ]; then
+    echo "Usage: ./Dodger.sh {interface}"
+    exit 1
+fi
+
+# Check if device exists
+ip address show dev $1
+if [ $? -eq 1 ]; then
+  echo "Please provide a valid interface"
+  exit 1
+fi
+
+
+
+# Drop old IP and wait a bit
+echo "Dropping IP..."
+dhclient -r $1
+if [ $? -eq 1 ]; then
+  echo "Releasing ip on device $1 failed. Please try again."
+  exit 1
+fi
+sleep 4
+
+# Set input interface down, then set random MAC address and hostname
+echo "Randomizing MAC & hostname.."
+ip link set $1 down
+macchanger -r $1
+if [ $? -eq 1 ]; then
+  echo "Randomizing MAC on device $1 failed"
+  exit 1
+fi
+
+hostnamectl hostname $(openssl rand -base64 12)
+if [ $? -eq 1 ]; then
+  echo "Randomizing hostname on device $1 failed"
+  exit 1
+fi
+
+
+# Set input interface up, renew IP.
+echo "Renewing IP..."
+ip link set $1 up
+dhclient $1
+if [ $? -eq 1 ]; then
+  echo "Renewing ip on device $1 failed"
+  exit 1
+fi
+echo "Done!"
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Dodger-min
 A fork of Ryan "Ghost" Voit's Dodger.py
+
+Dodger is designed to quickly dodge basic MAC and hostname filtering without needing to take time to reset network configuration on your linux based computer. This fork is written in bash.
+
+# Dependencies
+
+In order for this script to work, simply have the following installed:
+
+dhclient
+hostnamectl
+macchanger 
+iproute2
+
+# Usage
+
+chmod +x Dodger.sh
+./Dodger.sh {interface}
+
+Example:
+./Dodger.sh wlan0
